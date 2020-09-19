139d257ba8cf3aa992a2a03ae850205149ea666c
lingerlin <1292861738@qq.com>
2020-09-20 00:09:11 +0800

    add Continuous login

--- src/main/java/com/lingerlin/community/community/controller/AuthorizeController.java
+++ src/main/java/com/lingerlin/community/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) { //session是从request中拿到，当将HttpServletRequest放在方法中，Spring就会自动从上下文Request
+                           HttpServletRequest request,//session是从request中拿到，当将HttpServletRequest放在方法中，Spring就会自动从上下文Request
+                           HttpServletResponse response) {
         AccessTokenDto accessTokenDto = new AccessTokenDto(); //ctrl+shift+v 能快速实例化一个对象
         //shift+enter能快速换行并让光标移到最前
         accessTokenDto.setCode(code);
@@ -51,14 +54,18 @@ public class AuthorizeController {
             //登录成功，写cookie和session
             User user = new User();
 
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();//抽出token的目的是为了让其代替cookie
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
 //            System.out.println("user.setName="+user.getName());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);//获取Session对象并将user对象放置在session中
+            response.addCookie(new Cookie("token",token));
+
+//            //登录成功，写入session和cookie
+//            request.getSession().setAttribute("user",githubUser);//获取Session对象并将user对象放置在session中
             /*
             此时相当于在银行中已经创建成功一个银行账户，但是并未发给前端一个银行卡
             如果不手动给账户签发一个银行卡，就会自动生成一个默认的卡，无法自定义
